f208db46ed6296b306ef296657ec8dceef17c1ee
SohailLuffy <sohailislammail97@gmail.com>
2022-11-14 21:02:09 +0530

    Check for option Ladder Snake or No Pay

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -5,10 +5,32 @@ namespace Day4SnakeAndLadder
     {
         static void Main(string[] args)
         {
+             {
+                int playerPosition = 0;
                 Random random = new Random();
                 int roll = random.Next(1, 7);
-
-                Console.WriteLine("The Player roll is: " + roll);
+                int option = random.Next(0, 3);
+                   switch (option)
+                {
+                    case 0:
+                         Console.WriteLine("Player choose not to play.");
+                        break;
+                    case 1:
+                        playerPosition += roll;
+                        Console.WriteLine("Player choose the ladder. Player new position is: {0}", playerPosition);
+                        break;
+                    case 2:
+                        playerPosition -= roll;
+                        if (playerPosition < 0)
+                        {
+                            playerPosition = 0;
+                        }
+                        Console.WriteLine("Player choose the snake. Player new position is: {0}", playerPosition);
+                        break;
+                }
             }
+
+
         }
     }
+}
