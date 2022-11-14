791822e8b933120fb3605c34cef9233e0f3eefb6
SohailLuffy <sohailislammail97@gmail.com>
2022-11-14 21:18:00 +0530

    Player exact winning at position 100

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,31 +1,37 @@
 ﻿
 namespace Day4SnakeAndLadder
 {
-           class UC4SnakeLadderGame
+           class UC5SnakeLadderGame
     {
         static void Main(string[] args)
         {
-
             int playerPosition = 0;
             Random random = new Random();
 
             while (playerPosition != 100)
             {
-                //generate roll
+
                 int roll = random.Next(1, 7);
-                //generate option
                 int option = random.Next(0, 3);
 
                 switch (option)
                 {
                     case 0:
-                        //no play
+                  
                         Console.WriteLine("Player choose not to play.");
                         break;
                     case 1:
-                        playerPosition += roll;
-                        Console.WriteLine("Player choose the ladder. Player new position is: {0}", playerPosition);
-                        //ladder
+                        if (playerPosition + roll > 100)
+                        {
+                            Console.WriteLine("Player choose not to play.");
+                        }
+                        else
+                        {
+                            playerPosition += roll;
+                            Console.WriteLine("Player choose the ladder. Player new position is: {0}", playerPosition);
+
+                        }
+             
                         break;
                     case 2:
                         playerPosition -= roll;
@@ -34,11 +40,10 @@ namespace Day4SnakeAndLadder
                             playerPosition = 0;
                         }
                         Console.WriteLine("Player choose the snake. Player new position is: {0}", playerPosition);
-                        //snake
+                
                         break;
                 }
             }
-
         }
     }
 }
